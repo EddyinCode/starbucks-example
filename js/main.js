b3609e11df2a3671e996d163225c95c4f75c04a7
EddyinCode <eddyinthecode@gmail.com>
2022-06-10 21:45:07 +0900

    notice 구성 및 swiper js를 활용한 슬라이드 만들기

--- js/main.js
+++ js/main.js
@@ -21,3 +21,34 @@ window.addEventListener('scroll', _.throttle(function (){
     if(window.scrollY > 500) gsap.to(badgeEl, .6, {opacity:0, display: 'none'})
     else gsap.to(badgeEl, .6, {opacity: 1, display: 'block'})
 }, 300))
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index){
+    gsap.to(fadeEl, 1, {delay: (index+1)*.7, opacity: 1,})
+})
+
+
+new Swiper('.notice-line .swiper-container', { direction: 'vertical', autoplay: true, loop: true })
+
+new Swiper('.promotion .swiper-container', { slidesPerView: 3, spaceBetween: 10, centeredSlides: true, loop: true,
+    autoplay: {delay: 5000},
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl:'.promotion .swiper-prev',
+        nextEl:'.promotion .swiper-next'
+    }
+
+})
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function (){
+    isHidePromotion = !isHidePromotion
+    if (isHidePromotion) promotionEl.classList.add('hide')
+    else promotionEl.classList.remove('hide')
+})
